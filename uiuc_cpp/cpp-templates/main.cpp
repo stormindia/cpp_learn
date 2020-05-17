8171b6190af74a9aee715aa0de84ba7d04df4e51
stormindia <bajpaiharshit14@gmail.com>
2020-05-17 12:44:54 +0000

    corrected template

--- uiuc_cpp/cpp-templates/main.cpp
+++ uiuc_cpp/cpp-templates/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+
 using std::cout;
 using std::endl;
 
@@ -7,18 +9,18 @@ using my_cube::Cube;
 
 
 template <typename T> 
-T max(T a, T b){
-if(a > b){
-return a;
-}
-return b;
+T my_max(T a, T b){
+if(a > b){ return a;}
+else{return b;}
 }
 
 
 int main() {
-  cout << "max(3, 5): " << max(3, 5) << endl;
-  cout << "max('a', 'd'): " << max('a', 'd') << endl;
-  cout << "max(\"Hello\", \"World\"): " << max("Hello", "World") << endl;
+  cout << "max(3, 5): " << my_max(3, 5) << endl;
+  cout << "max('a', 'd'): " << my_max('a', 'd') << endl;
+  cout << "max(\"Hello\", \"World\"): " << my_max("Hello", "World") << endl;
+  cout << "max(\"World\", \"Hello\"): " << my_max("World", "Hello") << endl;
+  cout << "max(\"World\", \"Hello\"): " << my_max(std::string("Hello"), std::string("World")) << endl;
 //  cout << "max( Cube(3), Cube(6) ): " << max( Cube(3), Cube(6) ) << endl;
 
   return 0;
